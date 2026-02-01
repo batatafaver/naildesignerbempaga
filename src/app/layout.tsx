9643eb4d3158c68a004b649639602cce832b0257
Victor Scarabelot <victor.scarabelot@gmail.com>
2026-02-01 15:05:30 +0000

    57 96 76 100 57 Performance 96 Accessibility 76 Best Practices 100 SEO T

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type {Metadata} from 'next';
-import Script from 'next/script';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { Header } from '@/components/Header';
@@ -30,28 +29,6 @@ export default function RootLayout({
         {children}
         <Footer />
         <Toaster />
-        <Script id="fb-pixel" strategy="lazyOnload">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '25674147322228129');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=25674147322228129&ev=PageView&noscript=1"
-          />
-        </noscript>
       </body>
     </html>
   );
